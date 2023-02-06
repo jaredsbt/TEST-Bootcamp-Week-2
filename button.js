382c9c9a905789157278ad6ac3b7064ae1ac9556
jaredsbt <jaredsbt@gmail.com>
2023-02-06 00:49:30 -0800

    Modified buttons withs JS; Images float right by the text

---
+++ button.js
@@ -0,0 +1,31 @@
+const btn = document.createElement('button');
+
+btn.innetText = 'Recipes';
+
+btn.addEventListener('Recipes', () => {
+    alert('Loading...')
+});
+
+document.body.appendChild(btn);
+//
+
+const ubeCheesecakeBtn = document.createElement('ubeCheesecakeBtn');
+
+ubeCheesecakeBtn.innetText = 'Ube Cheescake';
+
+ubeCheesecakeBtn.addEventListener('Ube Cheesecake', () => {
+    alert('Loading...')
+});
+
+document.body.appendChild(ubeCheesecakeBtn);
+//
+
+const cookiesBtn = document.createElement('Cookies and Cream Brownies');
+
+cookiesBtn.innetText = 'Recipes';
+
+cookiesBtn.addEventListener('Cookies and Cream Brownies', () => {
+    alert('Loading...')
+});
+
+document.body.appendChild(cookiesBtn);
---
+++ intro.js
@@ -0,0 +1,3 @@
+<nav>
+    <a src = "/list.html/">Recipes</a>
+</nav>
